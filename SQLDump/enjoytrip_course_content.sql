c5b2d947f5852454305b9c3612d3e841eee81e18
유승아 <ysa8497@gmail.com>
2023-05-02 16:18:03 +0900

    add sql dump

---
+++ SQLDump/enjoytrip_course_content.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: enjoytrip
+-- ------------------------------------------------------
+-- Server version	8.0.32
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `course_content`
+--
+
+DROP TABLE IF EXISTS `course_content`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `course_content` (
+  `content_id` int NOT NULL,
+  `course_id` int NOT NULL,
+  PRIMARY KEY (`content_id`,`course_id`),
+  KEY `fk_attraction_info_has_attraction_course_attraction_course1_idx` (`course_id`),
+  KEY `fk_attraction_info_has_attraction_course_attraction_info1_idx` (`content_id`),
+  CONSTRAINT `fk_attraction_info_has_attraction_course_attraction_course1` FOREIGN KEY (`course_id`) REFERENCES `attraction_course` (`course_id`),
+  CONSTRAINT `fk_attraction_info_has_attraction_course_attraction_info1` FOREIGN KEY (`content_id`) REFERENCES `attraction_info` (`content_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `course_content`
+--
+
+LOCK TABLES `course_content` WRITE;
+/*!40000 ALTER TABLE `course_content` DISABLE KEYS */;
+/*!40000 ALTER TABLE `course_content` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-05-02 16:15:51
