baf66b878ede818e13d678011468930d644bfe42
SEUNG-AH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2023-05-02 15:38:07 +0900

    Update enjoytrip_user.sql

--- SQLDump/enjoytrip_user.sql
+++ SQLDump/enjoytrip_user.sql
@@ -25,7 +25,7 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
-  `id` int NOT NULL,
+  `id` varchar(20) NOT NULL,
   `pw` varchar(20) DEFAULT NULL,
   `name` varchar(20) DEFAULT NULL,
   `role` varchar(20) DEFAULT NULL,
