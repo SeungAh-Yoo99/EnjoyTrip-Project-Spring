43e37dea21a4aa07bb99ccd0694e59a117056bbe
유승아 <ysa8497@gmail.com>
2023-05-02 14:31:58 +0900

    MOD AttractionController url

--- src/main/java/com/mvc/controller/AttractionController.java
+++ src/main/java/com/mvc/controller/AttractionController.java
@@ -18,13 +18,13 @@ public class AttractionController {
 	@Autowired
     AttractionService service;
 	
-	@GetMapping(value = "/attration/{sido_code}")
+	@GetMapping(value = "/attraction/{sido_code}")
     @ApiOperation(notes="지역별 관광지 정보 수집", value="sido_code 별 관광지 정보 수집")
     public List<Attraction> selectBySidoCode(@PathVariable String sido_code) throws Exception {
         return service.selectBySidoCode(sido_code);
     }
 	
-	@GetMapping(value = "/attration/{sido_code}/{content_type_id}")
+	@GetMapping(value = "/attraction/{sido_code}/{content_type_id}")
     @ApiOperation(notes="지역별 원하는 컨텐츠 별 관광지 정보 수집", value="sido_code & content_type_id 별 관광지 정보 수집")
     public List<Attraction> selectBySidoCodeAndContentTypeId(@PathVariable String sido_code, @PathVariable String content_type_id) throws Exception {
         return service.selectBySidoCodeAndContentTypeId(sido_code, content_type_id);
